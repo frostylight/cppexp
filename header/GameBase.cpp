51c948c1dcb3658d1f06e2bbcff6ec8a778406a3
810560476 <810560476@qq.com>
2021-12-05 12:27:08 +0800

    重设计类继承结构base

---
+++ header/GameBase.cpp
@@ -0,0 +1,128 @@
+#include "GameBase.hpp"
+
+template<typename T>
+T abs(const T &x) {
+    return x > 0 ? x : -x;
+}
+
+namespace Base {
+#pragma region object
+    object::object(cREAL &x, cREAL &y)
+      : _x(x), _y(y), _state(1) {}
+    REAL object::getx() const {
+        return _x;
+    }
+    REAL object::gety() const {
+        return _y;
+    }
+    void object::setx(cREAL &x) {
+        _x = x;
+    }
+    void object::sety(cREAL &y) {
+        _y = y;
+    }
+    void object::setxy(cREAL &x, cREAL &y) {
+        _x = x;
+        _y = y;
+    }
+    bool object::active() const {
+        return _state;
+    }
+    void object::disable() {
+        _state = 0;
+    }
+    bool object::inscreenB(cREAL &width, cREAL &height) const {
+        return (getw() >> 1) <= _x && _x + (getw() >> 1) <= width &&
+               (geth() >> 1) <= _y && _y + (geth() >> 1) <= height;
+    }
+    bool object::inscreen(cREAL &width, cREAL &height) const {
+        return 0 <= _x && _x <= width &&
+               0 <= _y && _y <= height;
+    }
+    bool object::collide(const object *const obj) const {
+        return Base::collide(this, obj);
+    }
+#pragma endregion
+
+    bool collide(const object *const obj1, const object *const obj2) {
+        return abs(obj1->_x - obj2->_x) <= ((obj1->getw() + obj2->getw()) >> 1) &&
+               abs(obj1->_y - obj2->_y) <= ((obj1->geth() + obj2->geth()) >> 1);
+    }
+
+#pragma region chara
+    chara::chara(cREAL &x, cREAL &y, cint &health)
+      : object(x, y), _health(health) {}
+    int chara::gethealth() const {
+        return _health;
+    }
+    void chara::sethealth(cint &health) {
+        _health = health;
+    }
+    void chara::hurt(cuint &damage) {
+        _health -= damage;
+    }
+    void chara::heal(cuint &health) {
+        _health += health;
+    }
+    bool chara::dead() const {
+        return _health <= 0;
+    }
+    bool chara::active() const {
+        return object::active() && _health > 0;
+    }
+#pragma endregion
+
+#pragma region player
+    player::player(cREAL &x, cREAL &y, cint &health, cREAL &speed)
+      : chara(x, y, health), _speed(speed) {}
+    REAL player::getspeed() const {
+        return _speed;
+    }
+    void player::setspeed(cREAL &speed) {
+        _speed = speed;
+    }
+#pragma endregion
+
+#pragma region enemy
+    enemy::enemy(cREAL &x, cREAL &y, cint &health)
+      : chara(x, y, health), _cd(0) {}
+    bool enemy::bulletReady() const {
+        return _cd == getbulletCD();
+    }
+    void enemy::update() {
+        if(_cd == getbulletCD())
+            _cd = 1;
+        else
+            ++_cd;
+    }
+#pragma endregion
+
+#pragma region drop
+    drop::drop(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy)
+      : object(x, y), _dx(dx), _dy(dy) {}
+    void drop::update() {
+        _x += _dx;
+        _y += _dy;
+    }
+#pragma endregion
+
+#pragma region bullet
+    bullet::bullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage)
+      : drop(x, y, dx, dy), _damage(damage) {}
+#pragma endregion
+
+#pragma region playerbullet
+    playerbullet::playerbullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage)
+      : bullet(x, y, dx, dy, damage) {}
+    playerbullet::playerbullet(const player &p, cREAL &dx, cREAL &dy, cuint &damage)
+      : bullet(p._x, p._y, dx, dy, damage) {}
+#pragma endregion
+
+#pragma region enemybullet
+    enemybullet::enemybullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage)
+      : bullet(x, y, dx, dy, damage) {}
+    enemybullet::enemybullet(const enemy &e, cREAL &dx, cREAL &dy, cuint &damage)
+      : bullet(e._x, e._y, dx, dy, damage) {}
+#pragma endregion
+
+} // namespace Base
---
+++ header/GameBase.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "pre.hpp"
+
+
+namespace Base {
+    class object {
+        friend bool collide(const object *const obj1, const object *const obj2);
+
+      protected:
+        REAL _x, _y;
+        bool _state;
+
+      public:
+        object(cREAL &x, cREAL &y);
+
+        REAL getx() const;
+        REAL gety() const;
+        void setx(cREAL &x);
+        void sety(cREAL &y);
+        void setxy(cREAL &x, cREAL &y);
+        virtual uint getw() const = 0;
+        virtual uint geth() const = 0;
+
+        virtual bool active() const;
+        void disable();
+
+        bool inscreenB(cREAL &width, cREAL &height) const;
+        bool inscreen(cREAL &width, cREAL &height) const;
+
+        virtual void update()     = 0;
+        virtual void draw() const = 0;
+
+        bool collide(const object *const obj) const;
+    };
+
+    bool collide(const object *const obj1, const object *const obj2);
+
+    class chara: public object {
+      protected:
+        int _health;
+
+      public:
+        chara(cREAL &x, cREAL &y, cint &health);
+
+        int gethealth() const;
+        void sethealth(cint &health);
+        void hurt(cuint &damage);
+        void heal(cuint &health);
+        bool dead() const;
+
+        virtual bool active() const override;
+    };
+
+    class player: public chara {
+        friend class playerbullet;
+
+      protected:
+        REAL _speed;
+
+      public:
+        player(cREAL &x, cREAL &y, cint &health, cREAL &speed);
+
+        REAL getspeed() const;
+        void setspeed(cREAL &speed);
+    };
+
+    class enemy: public chara {
+        friend class enemybullet;
+
+      protected:
+        uint _cd;
+        virtual uint getbulletCD() const = 0;
+
+      public:
+        enemy(cREAL &x, cREAL &y, cint &health);
+
+        virtual bool bulletReady() const;
+
+        virtual void update();
+    };
+
+    class drop: public object {
+      protected:
+        REAL _dx, _dy;
+
+      public:
+        drop(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy);
+
+        virtual void update() override;
+    };
+
+    class bullet: public drop {
+      protected:
+        uint _damage;
+
+      public:
+        bullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
+    };
+
+    class playerbullet: public bullet {
+      public:
+        playerbullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
+        playerbullet(const player &p, cREAL &dx, cREAL &dy, cuint &damage);
+    };
+
+    class enemybullet: public bullet {
+      public:
+        enemybullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
+        enemybullet(const enemy &e, cREAL &dx, cREAL &dy, cuint &damage);
+    };
+} // namespace Base
