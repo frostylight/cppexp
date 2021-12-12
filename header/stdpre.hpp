08cfa63560d66719fe7487b62836f14b2ab37e07
810560476 <810560476@qq.com>
2021-12-12 16:14:03 +0800

    微调

---
+++ header/stdpre.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+using sbyte     = signed char;
+using ushort    = unsigned short;
+using ulong     = unsigned long;
+using ulonglong = unsigned long long;
+using uint      = unsigned int;
+
+using REAL = float;
+
+using wchar = wchar_t;
+
+using wstring = const wchar *const;
+
+//浮点精度
+constexpr REAL Realpcs = 1e-4;
