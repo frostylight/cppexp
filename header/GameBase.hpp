f23bb2951cf95019e6c76c30b567f290b6e7264a
810560476 <810560476@qq.com>
2021-12-12 16:15:21 +0800

    重构，调整碰撞箱相关代码

--- header/GameBase.hpp
+++ header/GameBase.hpp
@@ -1,134 +1,79 @@
 #pragma once
+#include <cmath>
 
-#include "pre.hpp"
+#include "stdpre.hpp"
 
 
 namespace Base {
+    //所有物体的基类
     class object {
-        friend bool collide(const object *const obj1, const object *const obj2);
-
       protected:
-        //位置坐标
+        //物体中心坐标
         REAL _x, _y;
-        bool _state;
-
-        //获取碰撞箱宽度
-        virtual uint getw() const = 0;
-        //获取碰撞箱高度
-        virtual uint geth() const = 0;
 
       public:
-        object(cREAL &x, cREAL &y);
-
-        REAL getx() const;
-        REAL gety() const;
-        void setx(cREAL &x);
-        void sety(cREAL &y);
-        void setxy(cREAL &x, cREAL &y);
-
-        // object是否失效
-        virtual bool active() const;
-        //使object失效
-        void disable();
-
-        //碰撞箱是否在画面内
-        bool inscreenB(cREAL &width, cREAL &height) const;
-        //是否在画面内
-        bool inscreen(cREAL &width, cREAL &height) const;
+        object(const REAL &x, const REAL &y);
 
         //数据更新
         virtual void update() = 0;
-        //绘制object
+        //绘图
         virtual void draw() = 0;
-
-        //碰撞检测
-        bool collide(const object *const obj) const;
     };
 
-    //碰撞检测
-    bool collide(const object *const obj1, const object *const obj2);
-
-    class chara: public object {
+    //人物
+    class chara: virtual public object {
       protected:
-        //生命
+        //生命值
         int _health;
-        //发射弹幕CD计数
-        uint _cd;
-
-        //弹幕CD
-        virtual uint getbulletCD() const = 0;
 
       public:
-        chara(cREAL &x, cREAL &y, cint &health);
-
-        int gethealth() const;
-        void sethealth(cint &health);
-        void hurt(cuint &damage);
-        void heal(cuint &health);
-        //是否死亡
-        bool dead() const;
-
-        //能否发射弹幕
-        virtual bool bulletReady() const;
-
-        virtual bool active() const override;
-
-        //更新弹幕CD计数
-        virtual void update() override;
+        chara(const int &health);
     };
 
+    //玩家
     class player: public chara {
-        friend class playerbullet;
-
-      protected:
-        //移速 (像素每帧)
-        REAL _speed;
-
       public:
-        player(cREAL &x, cREAL &y, cint &health, cREAL &speed);
-
-        REAL getspeed() const;
-        void setspeed(cREAL &speed);
+        using chara::chara;
     };
 
+    //敌人
     class enemy: public chara {
-        friend class enemybullet;
-
       public:
-        enemy(cREAL &x, cREAL &y, cint &health);
+        using chara::chara;
     };
 
-    class drop: public object {
+    //物体
+    class item: virtual public object {
       protected:
-        //每帧坐标移动
+        //每帧位移
         REAL _dx, _dy;
 
       public:
-        drop(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy);
+        item(const REAL &dx, const REAL &dy);
 
-        //更新坐标
         virtual void update() override;
     };
 
-    class bullet: public drop {
+    //子弹
+    class bullet: public item {
       protected:
-        //弹幕伤害
+        //命中伤害
         uint _damage;
 
       public:
-        bullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
+        bullet(const REAL &dx, const REAL &dy, const uint &damage);
     };
 
+    //玩家子弹
     class playerbullet: public bullet {
       public:
-        playerbullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
-        playerbullet(const player *const p, cREAL &dx, cREAL &dy, cuint &damage);
+        using bullet::bullet;
     };
 
+    //敌人子弹
     class enemybullet: public bullet {
       public:
-        enemybullet(cREAL &x, cREAL &y, cREAL &dx, cREAL &dy, cuint &damage);
-        enemybullet(const enemy *const e, cREAL &dx, cREAL &dy, cuint &damage);
+        using bullet::bullet;
     };
 
 } // namespace Base
---
+++ header/GameInterface.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "GameBase.hpp"
+
+namespace Interface {
+    //碰撞箱接口
+    template<typename T>
+    class IBox: virtual public Base::object {
+      public:
+        //碰撞检测
+        template<typename U>
+        inline bool collide(const IBox<U> *const obj) const {
+            return 2 * abs(_x - obj->_x) - T::_boxwidth - U::_boxwidth <= Realpcs &&
+                   2 * abs(_y - obj->_y) - T::_boxheight - U::_boxheight <= Realpcs;
+        }
+        inline bool collide(const IBox<T> *const obj) const {
+            return abs(_x - obj->_x) - T::_boxwidth <= Realpcs &&
+                   abs(_y - obj->_y) - T::_boxheight <= Realpcs;
+        }
+        IBox() {}
+    };
+} // namespace Interface
