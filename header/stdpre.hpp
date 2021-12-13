3dfe1337a0c72944006025cdbe52c0c8b7f44b52
810560476 <810560476@qq.com>
2021-12-13 13:20:50 +0800

    将碰撞箱宽高修改为整型

--- header/stdpre.hpp
+++ header/stdpre.hpp
@@ -11,12 +11,3 @@ using REAL = float;
 using wchar = wchar_t;
 
 using wstring = const wchar *const;
-
-//浮点精度
-constexpr REAL Realpcs = 1e-4;
-
-//绝对值模板
-template<typename T>
-inline T abs(const T &x) {
-    return x > 0 ? x : -x;
-}
